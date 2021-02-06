e959051de40cf6069993fd0fdc5bf158af9a9772
summerisjason <qinguangsen3231@163.com>
2021-02-06 11:19:38 +0800

    内置cookie

--- jdCookie.js
+++ jdCookie.js
@@ -3,7 +3,7 @@
  */
 //此处填写京东账号cookie。
 let CookieJDs = [
-  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJf8liNADAIw0LjwapUmoQx8MiQBMMGroyb7yVKInz7KZkwXhCPm5MGA0XWgrPXsoBOo4qiJ9o; pt_pin=13138777671_p;',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断环境变量里面是否有京东ck
